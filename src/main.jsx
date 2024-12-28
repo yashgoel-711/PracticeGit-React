ba06f0a0157c0cf403bfc1cbdc6d6cdfa0649326
yashgoel711@gmail.com <yashgoel711@gmail.com>
2024-12-28 15:39:34 +0530

    final stage

--- src/main.jsx
+++ src/main.jsx
@@ -31,8 +31,7 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    
+  <StrictMode>    
     <RouterProvider router={router} /> 
   </StrictMode>,
 )
